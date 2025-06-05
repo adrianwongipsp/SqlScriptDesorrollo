36ca12791b72d4895254ea734c2cd86c1716b659
adrianwongipsp <103456853+adrianwongipsp@users.noreply.github.com>
2025-06-05 09:03:01 -0500

    cambios vistaas 20250605

--- QuerySyncJuvai/VistaJUVAI.sql
+++ QuerySyncJuvai/VistaJUVAI.sql
@@ -1,18 +1,22 @@
----pesos precrias
-create view Vw_BasePesoMuestra
+use IPSP_JUVAI
+go
+
+alter view Vw_BasePesoMuestra
 as 
 	select 
 	im.nbIdImagen,
 	im.nbSampleWeight,		im.nbIdTipoMuestra, im.nbIdCicloPiscina,     im.nbIdUsuario, 
 	im.txtUsuarioRegistro,  im.dtFechaRegistro, im.dtFechaRegistroEnvio ,im.txtNombreMuestra,
-	ar.nbAverageWeight,		ar.nbAverageLength,
+	ar.nbAverageWeight,		ar.nbAverageLength, ar.nbDias, 				ar.nbNumeroAnimales,
 	tm.txtNombreTipo as nombreTipoMuestra, 
 	tm.nbIdFase,
 	cp.txtIdPiscina, 
 	cp.txtRolPiscina, 
 	cp.txtEstadoCiclo, 
 	cp.nbIdPiscinaEjecucion , 
-	cp.nbNumeroCiclo
+	cp.nbNumeroCiclo,
+	cp.txtClavePiscina, 
+	cp.txtCodigoCiclo 
 	from [tbImagenMuestra] im 
 			inner join [tbCicloPiscina] cp on im.nbIdCicloPiscina = cp.nbIdCicloPiscina
 			inner join [tbTipoMuestra]  tm on tm.nbIdTipoMuestra = im.nbIdTipoMuestra
